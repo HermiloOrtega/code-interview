440ec4a53a0a436fcd48d78be89f420f3cd1b93e
HermiloOrtega <hermilo.jh@gmail.com>
2020-06-26 18:37:32 -0700

    Add Osensa ISE

---
+++ OsensaISE/OsensaISE/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  OsensaISE
+//
+//  Created by Jose Hermilo Ortega Martinez on 2020-06-26.
+//  Copyright © 2020 Jose Hermilo Ortega Martinez. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
